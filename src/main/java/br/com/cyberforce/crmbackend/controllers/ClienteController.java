41cbbd3f93e2f15b07d73c57ef6469208353a47a
Guilherme Borges <ti@dse.com.br>
2023-04-05 17:30:50 -0300

    Adicionado configuracao de CORS

--- src/main/java/br/com/cyberforce/crmbackend/controllers/ClienteController.java
+++ src/main/java/br/com/cyberforce/crmbackend/controllers/ClienteController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/clientes")
+@CrossOrigin
 public class ClienteController {
 
     @Autowired
--- src/main/java/br/com/cyberforce/crmbackend/controllers/ProjetoController.java
+++ src/main/java/br/com/cyberforce/crmbackend/controllers/ProjetoController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/projetos")
+@CrossOrigin
 public class ProjetoController {
 
     @Autowired
